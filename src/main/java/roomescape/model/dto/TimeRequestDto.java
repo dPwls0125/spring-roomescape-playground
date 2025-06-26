94c70080fa085a982a96934128bd92583065fead
kimYejin <kimphoby0125@gmail.com>
2025-06-26 19:40:31 +0900

    fix : setter 제거

--- src/main/java/roomescape/model/dto/TimeRequestDto.java
+++ src/main/java/roomescape/model/dto/TimeRequestDto.java
@@ -3,12 +3,10 @@ package roomescape.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalTime;
 
 @Getter
-@Setter
 public class TimeRequestDto {
 
     @JsonFormat(pattern = "HH:mm")
